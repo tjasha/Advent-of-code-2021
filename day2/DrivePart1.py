4ac0f7ee1aea3f1905265fd833285a02b6c5f40a
tjasha <tjasa.spes@gmail.com>
2021-12-19 19:57:53 +0100

    draving part1

--- day2/DrivePart1.py
+++ day2/DrivePart1.py
@@ -1,2 +1,16 @@
 input = open('input.txt').readlines()
 
+i = 0
+forward = 0
+depth = 0
+while i < len(input):
+    if input[i].startswith('forward'):
+        forward += int(input[i][-2])
+    else:
+        if input[i].startswith('up'):
+            depth -= int(input[i][-2])
+        else:
+            depth += int(input[i][-2])
+    i = i + 1
+
+print(forward * depth)
